efa9391aa31694e477908d3e2b6bf39cff96cc07
Andrey Evgl <evgl2004@yandex.ru>
2024-01-21 15:30:51 +0500

    Изменение 04. Настроена фильтрация по курсу, уроку и способу оплаты. Настроен порядок сортировки по дате платежа.

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from payment.models import Payment
+from payment.serializers import PaymentSerializer
+from rest_framework import generics
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ('course', 'lesson', 'pay_type')
+    ordering_fields = ('data_pay', )
