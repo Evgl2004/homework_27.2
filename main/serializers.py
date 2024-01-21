036745cc62ba6d04404b33107f48556defb2f40d
Andrey Evgl <evgl2004@yandex.ru>
2024-01-21 11:21:35 +0500

    Изменение 01. Внесены изменения в модель Уроков, добавлено поле связи с моделью Курсы. Для модели Курсы добавлено в сериализатор подсчет количества связанных уроков.

--- main/serializers.py
+++ main/serializers.py
@@ -4,10 +4,16 @@ from main.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lesson = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_count_lesson(self, instance):
+        if instance.lesson.count():
+            return instance.lesson.count()
+        return 0
 
 
 class LessonSerializer(serializers.ModelSerializer):
